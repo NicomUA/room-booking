6e61f797dfc25c1bb32e6eb9c95fec09087bd792
Mykyta Matsapura <macaniki@gmail.com>
2024-03-02 21:27:21 +0200

    redis cache

---
+++ libs/db/src/db.cache.middleware.ts
@@ -0,0 +1,25 @@
+import { Prisma } from '@prisma/client';
+import { createPrismaRedisCache } from 'prisma-redis-middleware';
+import Redis from 'ioredis';
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: parseInt(process.env.REDIS_PORT),
+});
+
+export const cacheMiddleware: Prisma.Middleware = createPrismaRedisCache({
+  models: [
+    { model: 'Booking', excludeMethods: ['findMany'] },
+    { model: 'User', excludeMethods: ['findMany'] },
+    { model: 'Room', excludeMethods: ['findMany'] },
+  ],
+  storage: {
+    type: 'redis',
+    options: {
+      client: redis,
+      invalidation: { referencesTTL: 300 },
+    },
+  },
+  excludeModels: ['Booking'],
+  cacheTime: 300,
+});
--- libs/db/src/db.service.ts
+++ libs/db/src/db.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { cacheMiddleware } from './db.cache.middleware';
 
 @Injectable()
 export class DbService extends PrismaClient {
   constructor() {
     super();
+    this.$use(cacheMiddleware);
   }
 }
